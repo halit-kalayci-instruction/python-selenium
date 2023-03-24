f724ed51452a57ef18407ce3da30dbe4fc86d381
halitkalayci <halit.kalayci@ext.etiya.com>
2023-03-24 21:42:09 +0300

    WebDriverWait & ActionChains

--- test_sauce.py
+++ test_sauce.py
@@ -2,29 +2,50 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from selenium.webdriver.common.by import By
-
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.action_chains import ActionChains
 class Test_Sauce:
+    def __init__(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.maximize_window()
+        self.driver.get("https://www.saucedemo.com/")
+        
     def test_invalid_login(self):
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver.maximize_window()
-        driver.get("https://www.saucedemo.com/")
-        sleep(5)
         # en fazla 5 saniye olacak şekilde user-name id'li elementin görünmesini bekle
-        usernameInput = driver.find_element(By.ID, "user-name")
-        passwordInput = driver.find_element(By.ID,"password")
-        sleep(2)
+        WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        usernameInput = self.driver.find_element(By.ID, "user-name")
+        WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput = self.driver.find_element(By.ID,"password")
         usernameInput.send_keys("1")
         passwordInput.send_keys("1")
-        sleep(2)
-        loginBtn = driver.find_element(By.ID,"login-button")
-        sleep(2)
+        loginBtn = self.driver.find_element(By.ID,"login-button")
         loginBtn.click()
-        errorMessage = driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
         testResult = errorMessage.text == "Epic sadface: Username and password do not match any user in this service"
         print(f"TEST SONUCU: {testResult}")
+    def test_valid_login(self):
+        self.driver.get("https://www.saucedemo.com/")
+        WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        usernameInput = self.driver.find_element(By.ID, "user-name")
+        WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput = self.driver.find_element(By.ID,"password")
+        # Action Chains
+        actions = ActionChains(self.driver)
+        actions.send_keys_to_element(usernameInput, "standard_user")
+        actions.send_keys_to_element(passwordInput,"secret_sauce")
+        actions.perform()
+        #usernameInput.send_keys("standard_user")
+        #passwordInput.send_keys("secret_sauce")
+        loginBtn = self.driver.find_element(By.ID,"login-button")
+        loginBtn.click()
+        
+
+
+
 
 
 
 testClass = Test_Sauce()
 testClass.test_invalid_login()
+testClass.test_valid_login()
